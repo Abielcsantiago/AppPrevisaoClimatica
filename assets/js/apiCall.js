2b5fd750eb999fd0c843f0832987bb1136d8e552
Abiel <abielcsantiago@gmail.com>
2026-09-06 20:34:26 -0300

    Projeto base finalizado

--- assets/js/apiCall.js
+++ assets/js/apiCall.js
@@ -1,123 +1,50 @@
+import { setCurrentWeather, setWeatherForecast} from './setContent.js';
 const searchBttn = document.querySelector("#search-bttn");
-const hour = document.querySelector("#hour");
 searchBttn.addEventListener('click', getWeather);
-function toCelsius(n){
-    return (n - 273.15).toFixed(1)
-}
-function toKmHour(n){
-    return (n * 3.6).toFixed(1);
-}
-function setWallpaper(clima){
-    const body = document.body;
-    let img = "";
-    switch(clima){
-        case("Clouds"):
-            img = "lightCloudWallpaper.jpg"; 
-            break
-        case("Clear"):
-            img = "sunWallpaper.jpg";
-            break
-        case("Thunderstorm"):
-        case("Drizzle"):
-            img = "cloudyWallpaper.jpg";
-            break
-        case("Rain"):
-            img = "rainWallpaper.jpg";
-            break
-        default:
-            img = "lightCloudWallpaper.jpg"; 
-            break   
-        }
-    const cssDeclaration = `url("./assets/img/${img}")`
-    body.style.backgroundImage = cssDeclaration;
-    console.log(clima)
-}
-function setIcon(clima, element){
-    let icon = "";
-    switch(clima){
-        case("Clouds"):
-            icon = "iconCloud.png"; 
-            break
-        case("Clear"):
-            icon = "iconClear.png";
-            break
-        case("Thunderstorm"):
-        case("Drizzle"):
-            icon = "iconStorm.png";
-            break
-        case("Rain"):
-            icon = "iconRain.png";
-            break
-        default:
-            icon = "iconClear.png"; 
-            break   
-        } 
-        const cssDeclaration = `./assets/img/${icon}`;
-        element.setAttribute("src", cssDeclaration)  
-}
 async function weathearApi(lat, lon){
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ce11a71ed128dbb658dd43707d169cc8`) 
+    const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&forecast_days=7&daily=temperature_2m_mean,temperature_2m_max,temperature_2m_min,weather_code&current=temperature_2m,wind_speed_10m,apparent_temperature,relative_humidity_2m,weather_code`)
     const data = await res.json();
-    return data
-}
-function getLatAndLon(apiRes){
+    data.daily.temperature_2m_max.shift();
+    data.daily.temperature_2m_mean.shift();
+    data.daily.temperature_2m_min.shift();
+    data.daily.time.shift();
+    data.daily.weather_code.shift();
     return {
-        lat: apiRes[0].lat,
-        lon: apiRes[0].lon   
-    }
-}
-function getMainWeather(apiRes){
-    return {
-        temp: toCelsius(apiRes.main.temp),
-        feelsLike: toCelsius(apiRes.main.feels_like),
-        max: toCelsius(apiRes.main.temp_max),
-        min: toCelsius(apiRes.main.temp_min),
-        humidity: apiRes.main.humidity + "%"
+        current:{
+            temp: data.current.temperature_2m.toFixed(1) + " °",
+            humidity: data.current.relative_humidity_2m + "%",
+            feelsLike: data.current.apparent_temperature.toFixed(1) + " °",
+            windSpeed: data.current.wind_speed_10m + 'Km/h',
+            code: data.current.weather_code,
+        },
+        forecast:{
+            tempMax: data.daily.temperature_2m_max,
+            tempMean: data.daily.temperature_2m_mean,
+            tempMin: data.daily.temperature_2m_min,
+            date: data.daily.time,
+            code: data.daily.weather_code,
+        }
     }
 }
 async function geoApi(cidade){
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=1&appid=ce11a71ed128dbb658dd43707d169cc8`)
+    const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1&language=pt&format=json`);
     const data = await res.json();
-    const latAndLon = getLatAndLon(data)
-    return latAndLon;
-}
-function setContent(weather, mainWeather, windSpeed, city){
-    const wind = document.querySelector("#wind-value");
-    const feelsLike = document.querySelector("#feels-like-value");
-    const humidity = document.querySelector("#humidity-value");
-    const tempToday = document.querySelector("#temp-today");
-    const cityName = document.querySelector("#city-name");
-    const clima = weather.weather[0].main;
-    const icon = document.querySelector("#icon-temp-today");
-    wind.innerText = toKmHour(windSpeed);
-    feelsLike.innerText = mainWeather.feelsLike;
-    humidity.innerText = mainWeather.humidity;
-    tempToday.innerText = mainWeather.temp;
-    cityName.innerText = city;
-    setWallpaper(clima)
-    setIcon(clima, icon)
+    const results = data.results[0];
+    return {
+        lat: results.latitude,
+        lon: results.longitude
+    };
 }
 async function getWeather(e){
-    if(e){
-        e.preventDefault();    
-    }
+    if(e) e.preventDefault();    
     const city = document.querySelector("#input-city").value;
-    const geoRes = await geoApi(city);
-    const weather = await weathearApi(geoRes.lat, geoRes.lon)
-    //const forecast = await forecastApi(geoRes.lat, geoRes.lon)
-    const mainWeather = getMainWeather(weather);
-    const windSpeed = weather.wind.speed;
-    setContent(weather, mainWeather, windSpeed, city);
+    const latLon = await geoApi(city);
+    const weather = await weathearApi(latLon.lat, latLon.lon);
+    const current = weather.current;
+    const forecast = weather.forecast;
+    setCurrentWeather(current.temp, current.feelsLike, current.humidity, current.windSpeed, current.code, city);
+    console.log(forecast)
+    setWeatherForecast(forecast.tempMax, forecast.tempMin, forecast.tempMean, forecast.date, forecast.code);
 }
-function getHour(){
-    const today = new Date();
-    hour.innerText = `${today.getHours()}:${today.getMinutes()}`
-}
-async function forecastApi(lat, lon){
-    const res = await fetch(`api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=6&appid=ce11a71ed128dbb658dd43707d169cc8`);
-    const data = await res.json();
-    console.log(data)
-}
-const setHour = setInterval(getHour, 1000)
 document.querySelector("#input-city").value = "Camaçari";
-getWeather()
+getWeather();
---
+++ assets/js/setContent.js
@@ -0,0 +1,135 @@
+export {setCurrentWeather, setWeatherForecast}; 
+const clock = document.querySelector("#clock");
+function getHour(){
+    const today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    clock.innerText = `${hours}:${minutes}`;
+}
+function setIcon(clima, element){
+    let icon = "";
+    switch(clima){
+        case("Clouds"):
+            icon = "iconCloud.png"; 
+            break
+        case("Clear"):
+            icon = "iconClear.png";
+            break
+        case("Thunderstorm"):
+        case("Drizzle"):
+            icon = "iconStorm.png";
+            break
+        case("Rain"):
+            icon = "iconRain.png";
+            break
+        default:
+            icon = "iconClear.png"; 
+            break   
+        } 
+        const cssDeclaration = `./assets/img/${icon}`;
+        element.setAttribute("src", cssDeclaration)  
+}
+function setWallpaperAndIcon(code, iconElement){
+    let wallpaper = "";
+    let icon = "";
+    const clear = [0,1];
+    const cloudy = [2,3,45,48];
+    const lightRain = [51,53,56,61,63,66,80,81];
+    const heavyRain = [55,57,65,67,82,95,96,99];
+    const snow = [71,73,75,77,85,86];
+    if(clear.includes(code)){
+        wallpaper = "sunWallpaper.jpg"; 
+        icon = "iconClear.png";
+    }else if(cloudy.includes(code)){
+        wallpaper = "lightCloudWallpaper.jpg";
+        icon = "iconCloud.png";      
+    }else if(lightRain.includes(code)){
+        wallpaper = "rainWallpaper.jpg";
+        icon = "iconRain.png"; 
+    }else if(heavyRain.includes(code)){
+        wallpaper = "cloudyWallpaper.jpg";
+        icon = "iconStorm.png";     
+    }else{
+        wallpaper = "snowWallpaper.jpg"; 
+        icon = "iconSnow.png";    
+    }
+    iconElement.setAttribute("src", `./assets/img/${icon}`);
+    document.body.style.backgroundImage = `url("./assets/img/${wallpaper}")`;
+}
+function setCurrentWeather(temp, feelsLike, humidity, windSpeed, code, city){
+    const windElement = document.querySelector("#wind-value");
+    const feelsLikeElement = document.querySelector("#feels-like-value");
+    const humidityElement = document.querySelector("#humidity-value");
+    const tempElement = document.querySelector("#temp-today");
+    const cityElement = document.querySelector("#city-name");
+    const iconElement = document.querySelector("#icon-temp-today");
+    windElement.innerText = windSpeed;
+    feelsLikeElement.innerText = feelsLike;
+    humidityElement.innerText = humidity;
+    tempElement.innerText = temp;
+    cityElement.innerText = city;
+    setWallpaperAndIcon(code, iconElement);
+}
+function setWeatherForecast(max, min, mean, date, code){
+    const listForecast = document.querySelectorAll(".forecast-day");
+    listForecast.forEach(setForecastContent)
+    
+    function setForecastContent(listItem, i, array){
+        const dayWeak = listItem.querySelector(".day-weak");
+        const iconElement = listItem.querySelector(".icon");
+        const maxValue = listItem.querySelector(".max");
+        const minValue = listItem.querySelector(".min"); 
+        const progress = listItem.querySelector(".progress"); 
+        dayWeak.innerText = getDayWeak(date[i]);
+        maxValue.innerText = max[i].toFixed(1) + "°";
+        minValue.innerText = min[i].toFixed(1) + "°";
+        const percentProgress = (mean[i] / max[i]) * 100;
+        progress.style.marginLeft = `calc(${percentProgress}% - 30px)`;
+        progress.setAttribute("title", mean[i].toFixed(1) + "°")
+        progress.style.setProperty("--contentBefore", `"${mean[i].toFixed(1)}°"` )
+        setIconForecast(iconElement, code[i])
+    }
+}
+function setIconForecast(element, code){
+    let icon = "";
+    const clear = [0,1];
+    const cloudy = [2,3,45,48];
+    const lightRain = [51,53,56,61,63,66,80,81];
+    const heavyRain = [55,57,65,67,82,95,96,99];
+    const snow = [71,73,75,77,85,86];
+    if(clear.includes(code)){
+        icon = "iconClear.png";
+    }else if(cloudy.includes(code)){
+        icon = "iconCloud.png";      
+    }else if(lightRain.includes(code)){
+        icon = "iconRain.png"; 
+    }else if(heavyRain.includes(code)){
+        icon = "iconStorm.png";     
+    }else{
+        icon = "iconSnow.png";    
+    }
+    element.setAttribute("src", `./assets/img/${icon}`); 
+}
+function getDayWeak(date){
+    const fullDate = new Date(date);
+    const day = fullDate.getDay();
+    switch(day){
+        case(0):
+            return "Mon";
+        case(1):
+            return "Tue";
+        case(2):
+            return "Wed";
+        case(3):
+            return "Thu";
+        case(4):
+            return "Fri";
+        case(5):
+            return "Sat";
+        default:
+            return "Sun";
+    }
+}
+setInterval(getHour, 1000)
